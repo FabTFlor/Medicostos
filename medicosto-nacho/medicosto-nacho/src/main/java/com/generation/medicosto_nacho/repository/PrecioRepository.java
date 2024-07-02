2d304d2cade89761f1f22f3221bf284fb1d9d011
FabTFlor <fabiantorresflor@gmail.com>
2024-07-02 16:34:21 -0400

    vaciar main

--- medicosto-nacho/medicosto-nacho/src/main/java/com/generation/medicosto_nacho/repository/PrecioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.generation.medicosto_nacho.repository;
-
-import com.generation.medicosto_nacho.models.Precio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrecioRepository extends JpaRepository<Precio, Long> {
-}
